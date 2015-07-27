01e90012f3ecd4d056bf7e4ef2bb3f57ca84f4c4
Caleb Cox <calebcox314@gmail.com>
2015-07-26 22:09:48 -0500

    Use es6-collections polyfill on iOS

--- www/js/boot.js
+++ www/js/boot.js
@@ -7,7 +7,8 @@ require.config({
     'jquery': '../bower_components/jquery/jquery',
     'jquery-mobile': '../bower_components/jquery-mobile/jquery.mobile-1.4.5',
     'can': '../bower_components/canjs/amd/can',
-    'chance': '../bower_components/chance/chance'
+    'chance': '../bower_components/chance/chance',
+    'es6-collections': '../bower_components/es6-collections/index'
   },
   shim: {
     'jquery-mobile': {
@@ -19,6 +20,9 @@ require.config({
 
 define(function(require) {
   'use strict';
+
+  // Load the es6-collections polyfill
+  require('es6-collections');
 
   // Load and run the application
   var app = require('app');
